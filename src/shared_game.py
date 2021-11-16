cfcfac00dc3824cd672df0d101299c8112b75b14
sheepy0125 <sheepy404@gmail.com>
2021-11-16 18:42:48 -0500

    Snake reset and move (& some more)

--- src/shared_game.py
+++ src/shared_game.py
@@ -19,10 +19,11 @@ class SharedGame:
     Must be the same between the client and the server!
     """
 
+    grid_snap: int = 10
     window_width: int = 800
     window_height: int = 600
-    height: int = window_width // 10
-    width: int = window_height // 10
+    height: int = window_width // grid_snap
+    width: int = window_height // grid_snap
 
 
 class BaseSnakePlayer:
@@ -40,11 +41,15 @@ class BaseSnakePlayer:
         default_length: int = 1,
         identifier: int | str = "unknown snake",
     ):
+        # For resetting
+        self.default_pos = default_pos
+        self.default_tail_length = default_length
+
         self.identifier = identifier
-        self.pos = default_pos
+        self.alive = True
         self.tail: list[tuple] = [default_pos]  # List of positions
         self.tail_length = default_length
-        self.alive = True
+        self.pos = default_pos
 
         self.direction = "right"
         self.direction_velocity_enum = {
@@ -58,7 +63,15 @@ class BaseSnakePlayer:
             Logger.log(f"Snake {self.identifier} created")
 
     def move(self):
-        ...
+        # Update position
+        for dimension in range(2):
+            self.pos[dimension] = (
+                self.pos[dimension]
+                + self.direction_velocity_enum[self.direction][dimension]
+            )
+
+        self.tail.insert(0, self.pos)
+        self.tail.pop()
 
     def collision_checking(self, other_snake_object: "BaseSnakePlayer"):
         # Check if the snake is out of bounds
@@ -78,7 +91,19 @@ class BaseSnakePlayer:
                 self.snake_died(reason="collided with other snake")
 
     def snake_died(self, reason: str = "unknown"):
-        Logger.warn(
-            "This shouldn't be called!! Read the docstring for BaseSnakePlayer for why"
-        )
-        Logger.info(f"Snake {self.identifier} died because {reason = }")
+        if self == BaseSnakePlayer:  # If not derived at all
+            Logger.warn(
+                "This shouldn't be called! Supposed to use inheritence from this class  "
+                "to make the code specific to what end this is on (client or server)"
+            )
+
+        Logger.log(f"Snake {self.identifier} died because {reason = }")
+        self.alive = False
+
+        # Typically in a normal snake game, we'd need to reset everything
+        # now. But, since this is multiplayer snake, the server will handle
+        # that.
+
+    def reset(self):
+        # Just re-initialize the snake, nothin' fancy
+        self.__init__(self.default_pos, self.default_tail_length, self.identifier)
